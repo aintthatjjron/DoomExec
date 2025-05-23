63a9cac71e54e8de13bfbceaf4199aa68a85591b
kaytothe3 <126303901+aintthatjjron@users.noreply.github.com>
2025-05-23 11:25:13 -0400

    tryingbro

---
+++ bypass/stealth.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+#include <psapi.h>
+#include <iostream>
+
+void hideFromYara() {
+    std::cout << "[*] Hiding memory from YARA..." << std::endl;
+    // now we just have to actually figure this bs out
+}
