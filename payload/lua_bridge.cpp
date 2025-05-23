b7878521e3264bcb9de013ac6e96896aee15b982
kaytothe3 <126303901+aintthatjjron@users.noreply.github.com>
2025-05-23 11:24:24 -0400

    skeletonforhooking

--- payload/lua_bridge.cpp
+++ payload/lua_bridge.cpp
@@ -1,1 +1,8 @@
-// TODO: Bridge Lua C API to Roblox
+#include "lua_bridge.hpp"
+#include <iostream>
+
+// strange lua C API function (idk what the actual roblox luau hooking is)
+void attachToLuau() {
+    std::cout << "[*] Hooking into Luau..." << std::endl;
+    // now i just have to find lua state, hook instructions, andd probably replace dispatchers
+}
