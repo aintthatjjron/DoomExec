08fc9c6144f89ea3260af1b56d14f52e466830b0
kaytothe3 <126303901+aintthatjjron@users.noreply.github.com>
2025-05-23 11:26:45 -0400

    skeletonUGH

---
+++ utils/pattern_scan.cpp
@@ -0,0 +1,8 @@
+#include "pattern_scan.hpp"
+#include <windows.h>
+#include <psapi.h>
+
+uintptr_t patternScan(const char* pattern, const char* mask) {
+    // TODO get the stupid signature scanning
+    return 0;
+}
